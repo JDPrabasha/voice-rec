296b135d593819d4736af70b7921ca892fea3168
Dulaj <shad3s.of.m3@gmail.com>
2022-05-06 09:19:53 +0530

    complete voice recognition

--- src/App.js
+++ src/App.js
@@ -7,33 +7,47 @@ import * as speech from "@tensorflow-models/speech-commands";
 function App() {
   const [model, setModel] = useState(null);
   const [action, setAction] = useState(null);
-  const [label, setLabel] = useState(null);
+  const [labels, setLabels] = useState(null);
 
   const loadModel = async () => {
     const recognizer = await speech.create("BROWSER_FFT");
     console.log("Model loaded");
     await recognizer.ensureModelLoaded();
     console.log(recognizer.wordLabels());
+    setModel(recognizer);
+    setLabels(recognizer.wordLabels());
   };
 
   useEffect(() => {
     loadModel();
   }, []);
+
+  function argMax(arr) {
+    return arr.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+  }
+
+  const recognizeCommands = async () => {
+    console.log("Listening...");
+    model.listen(
+      (result) => {
+        console.log(result);
+        setAction(labels[argMax(Object.values(result.scores))]);
+        console.log(action);
+      },
+      { includeSpectrogram: true, probabilityThreshold: 0.75 }
+    );
+    // setTimeout(() => {
+    //   model.stopListening();
+    // }, 10e3);
+  };
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Your Wish Is My Command</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick={recognizeCommands}>Recognize</button>
+        <h3>{action ? action : "I can't hear you yet"}</h3>
       </header>
     </div>
   );
